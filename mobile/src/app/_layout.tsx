a34309ca6a18b6c2773e31d4803c899e41ac966e
naoya84 <naoya0517.mc@gmail.com>
2025-01-29 15:21:02 +0900

    mobileApp構築

---
+++ mobile/src/app/_layout.tsx
@@ -0,0 +1,20 @@
+import {Stack} from "expo-router";
+
+const Layout = () => {
+  return (
+    <Stack screenOptions={{
+      headerStyle: {
+        backgroundColor: '#467FD3'
+      },
+      headerTintColor: '#FFFFFF',
+      headerTitle: 'RungramReWrite',
+      headerBackTitle: 'Back',
+      headerTitleStyle: {
+        fontSize: 22,
+        fontWeight: 'bold'
+      }
+    }} />
+  )
+}
+
+export default Layout
---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,7 @@
+import {Redirect} from "expo-router";
+
+const Index = () => {
+  return <Redirect href='auth/LogIn'></Redirect>
+}
+
+export default Index
